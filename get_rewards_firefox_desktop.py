8dc38f9507be704d95b8ac2a34157f1630c49b11
Prateek Mahendrakar <prateek.mahendrakar@gmail.com>
2019-09-04 12:27:46 -0400

    firefox-desktop added

---
+++ get_rewards_firefox_desktop.py
@@ -0,0 +1,52 @@
+import json
+import random
+import time
+
+import requests
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+def wait_for(sec=2):
+    time.sleep(sec)
+
+
+randomlists_url = "https://www.randomlists.com/data/words.json"
+response = requests.get(randomlists_url)
+words_list = random.sample(json.loads(response.text)['data'], 60)
+print('{0} words selected from {1}'.format(len(words_list), randomlists_url))
+
+
+profile = webdriver.FirefoxProfile()
+#profile.set_preference("general.useragent.override","Mozilla/5.0 (Android 6.0.1; Mobile; rv:63.0) Gecko/63.0 Firefox/63.0")
+driver = webdriver.Firefox(firefox_profile=profile, executable_path='geckodriver.exe')
+
+try:
+    elem = driver.find_element_by_name('loginfmt')
+    elem.clear()
+    elem.send_keys("your-email-id") # add your login email id
+    elem.send_keys(Keys.RETURN)
+    wait_for(5)
+    elem1 = driver.find_element_by_name('your-password')
+    elem1.clear()
+    elem1.send_keys("your_password") # add your password
+    elem1.send_keys(Keys.ENTER)
+    wait_for(7)
+ 
+except Exception as e:
+    print(e)
+    wait_for(4)
+
+
+url_base = 'http://www.bing.com/search?q='
+
+wait_for(5)
+
+for num, word in enumerate(words_list):
+    print('{0}. URL : {1}'.format(str(num + 1), url_base + word))
+    try:
+        driver.get(url_base + word)
+        print('\t' + driver.find_element_by_tag_name('h2').text)
+    except Exception as e1:
+        print(e1)
+    wait_for()
+driver.close()
