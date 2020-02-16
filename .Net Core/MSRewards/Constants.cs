506288e5875a8c01a28b72874ce73616bba3cc2a
Prateek Mahendrakar <prateek.mahendrakar@gmail.com>
2020-02-16 15:16:40 -0500

    Added .Net Core version (#1)

---
+++ .Net Core/MSRewards/Constants.cs
@@ -0,0 +1,33 @@
+namespace MSRewards
+{
+    public static class Constants
+    {
+
+        public const string LiveLoginUrl = "https://login.live.com";
+
+        public const string LoginEntryName = "loginfmt";
+
+        public const string PasswordEntryName = "passwd";
+
+        public const string CheckboxName = "KMSI";
+
+        public const string RewardsPageTitle = "Microsoft account | Home";
+
+        public const string WordsListUrl = "https://raw.githubusercontent.com/bevacqua/correcthorse/master/wordlist.json";
+
+        public const string BingSearchURL = "http://www.bing.com/search?q=";
+
+        public const string GeckoPathKey = "webdriver.gecko.driver";
+
+        public const string UserAgentKey = "general.useragent.override";
+
+        public const string MobileUserAgent = @"Mozilla/5.0 (Android 9; Mobile; rv:74.0) Gecko/74.0 Firefox/74.0";
+
+        public const string PrivateBrowingKey = "browser.privatebrowsing.autostart";
+
+        public const string PointsBreakdownUrl = "https://account.microsoft.com/rewards/pointsbreakdown";
+
+
+    }
+
+}
