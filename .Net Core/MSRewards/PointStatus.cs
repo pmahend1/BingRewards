a9f70c49eaf61342081b1c5e2c49ca3293b1a7ae
Prateek Mahendrakar <prateek.mahendrakar@gmail.com>
2021-02-17 23:01:02 -0500

    updated nugets minor code fixes

---
+++ .Net Core/MSRewards/PointStatus.cs
@@ -0,0 +1,15 @@
+﻿namespace MSRewards
+{
+    public class PointStatus
+    {
+        public int Current { get; set; }
+
+        public int Maximum { get; set; }
+
+        public PointStatus(int current, int max)
+        {
+            this.Current = current;
+            this.Maximum = max;
+        }
+    }
+}
